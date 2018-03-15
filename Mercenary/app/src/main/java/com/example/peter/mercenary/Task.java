d177b263e218972401a4dd022e886cfe344ddd9f
Jmleung <jmleung@ualberta.ca>
2018-03-15 17:25:15 -0600

    Organization & Limits
    Organized functiosn into gets and sets for editing and retreival. Setup the charecter limits to throw exceptions for title and description.

--- Mercenary/app/src/main/java/com/example/peter/mercenary/Task.java
+++ Mercenary/app/src/main/java/com/example/peter/mercenary/Task.java
@@ -6,6 +6,7 @@ import io.searchbox.annotations.JestId;
 
 /**
  * Created by peter on 2018-02-22.
+ * Editted by Jason L. and Shardul S. on 2018-05-15
  */
 
 public class Task {
@@ -14,40 +15,64 @@ public class Task {
     private BidList listBids;
     private float geoLoc;
     private byte picture;
-    private String taskStatus;
+    private String status;
     @JestId
     private String id;
 
-    public String getId() { return  id; }
-    public void setId(String id) { this.id = id; }
+    public String getId() {
+        return id;
+    }
 
-    public Task (String title, String description, String taskStatus ){
-        this.title=title;
-        this.description=description;
-        this.taskStatus=taskStatus;
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Task(String title, String description, String status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
 
     }
 
+    public void setTitle(String title) {
 
-    public void addPicture(byte picture){
+        if (title.length() > 30) {
+            throw new TitleTooLongException();
+        }
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+
+        if (description.length() > 300) {
+            throw new DescTooLongException();
+        }
+        this.description = description;
+
+    }
+    public void setPhoto(byte picture){
         this.picture=picture;
     }
+    public void setGeo(float geoLoc){ this.geoLoc=geoLoc;}
+    public void setStatus(String status){this.status=status;}
 
-    public void addGeo(float geoLoc){ this.geoLoc=geoLoc;}
+    public String getTitle(){return this.title;}
+    public String getDescription(){return this.description;}
+    public byte getPhoto(){return this.picture;}
+    public float getGeoLoc(){return this.geoLoc;}
+    //public void getStatus(String taskStatus){this.status = status;}
+    public String getStatus(){return this.status;}
 
-    public void modDescription(String description){
-        this.description=description;
-    }
 
+    //public BidList getBids(){return this.listBids;}
+
+
+    /* Save this for subclass "MyTasks"
     public void addBid(Bid bid){
         this.listBids.add(bid);
     }
-
-    public void modTitle(String title){
-        this.title=title;
-    }
-
-    public void delBid(Bid bid){this.listBids.delBid(bid);}
+    public void delBid(Bid bid){
+        this.listBids.delBid(bid);}
 
     public float getLowestBid() {
         float value=-1;
@@ -66,18 +91,6 @@ public class Task {
         }
         return value;
     }
+    */
 
-    public byte getPicture(){return this.picture;}
-
-    public String getTitle(){return this.title;}
-
-    public String getDescription(){return this.description;}
-
-    public BidList getBids(){return this.listBids;}
-
-    public float getGeoLoc(){return this.geoLoc;}
-
-    public void modTaskStatus(String taskStatus){this.taskStatus=taskStatus;}
-
-    public String getTaskStatus(){return this.taskStatus;}
 }
