b3b44bfdda17a282d0a4f6a6dd837ca61c04ca6f
Shardul <shardul@ualberta.ca>
2018-03-18 20:48:47 -0600

    Javadoc for some exception classes and some of the newer classes. Still need to finish massive javadoc for User

--- Mercenary/app/src/main/java/com/example/peter/mercenary/UsernameTooLongException.java
+++ Mercenary/app/src/main/java/com/example/peter/mercenary/UsernameTooLongException.java
@@ -2,7 +2,13 @@ package com.example.peter.mercenary;
 
 /**
  * Created by Melissa on 2018-03-18.
+ * @date: 2018-03-18
+ * @author Melissa B.
+ * @version version 1.0
+ * @see Signup
+ * Note: class for sign up and log in UI
  */
+
 
 public class UsernameTooLongException extends Exception {
 }
