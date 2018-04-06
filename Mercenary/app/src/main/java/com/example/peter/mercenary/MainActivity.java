7266ef71bbd67e4edc95940790e7b1ca7a51d26e
Coursaux <corso77@live.com>
2018-04-05 20:26:09 -0600

    navigation *should* be working

--- Mercenary/app/src/main/java/com/example/peter/mercenary/MainActivity.java
+++ Mercenary/app/src/main/java/com/example/peter/mercenary/MainActivity.java
@@ -185,20 +185,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_user_profile) {
+            startActivity(new Intent(this, UserProfile.class));
+        } else if (id == R.id.nav_task_list) {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
--- Mercenary/app/src/main/java/com/example/peter/mercenary/UserProfile.java
+++ Mercenary/app/src/main/java/com/example/peter/mercenary/UserProfile.java
@@ -1,8 +1,14 @@
 package com.example.peter.mercenary;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
@@ -14,7 +20,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class UserProfile extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     User curUser; //the user currently logging in
     User pUser; //the user whose profile is being looked at
 
@@ -29,7 +35,7 @@ public class UserProfile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
+        setContentView(R.layout.drawer_layout_userprofile);
 
         curUser = getIntent().getParcelableExtra("user");
         pUser = getIntent().getParcelableExtra("clicked_user");
@@ -64,6 +70,18 @@ public class UserProfile extends AppCompatActivity {
             reviewsList.setAdapter(adapter);
 
         }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_userprofile);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_userprofile);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -75,5 +93,20 @@ public class UserProfile extends AppCompatActivity {
             phoneText.setText(pUser.getPhoneNumber());
             ratings.setRating(pUser.getRating());
         }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_user_profile) {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_userprofile);
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_task_list) {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+        return true;
     }
 }
